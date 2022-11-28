43441d5bbfe2922ba5742997dbb95df4e383e2b9
keru <45701634+kerufu@users.noreply.github.com>
2022-11-28 03:33:43 -0800

    Update checkDB.py

--- myP/checkDB.py
+++ myP/checkDB.py
@@ -1,5 +1,4 @@
 import sqlite3
 connection = sqlite3.connect("db.sqlite3")
-# cursor = connection.execute('select * from images_pi')
 cursor = connection.execute('select * from images_isp')
 print(cursor.fetchall())
