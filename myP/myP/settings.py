0bacc16ee7460569176ed036e5d8eb7b975a7f40
keru <45701634+kerufu@users.noreply.github.com>
2023-02-18 00:18:59 -0800

    Update settings.py
    change p path

--- myP/myP/settings.py
+++ myP/myP/settings.py
@@ -125,10 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+PICTURE_DIR = "/Users/richard/Documents/.P"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/Users/richard/Documents/.P/image',
-    '/Users/richard/Documents/.P/generatedImage'
+    os.path.join(PICTURE_DIR, '/image'),
+    os.path.join(PICTURE_DIR, '/generatedImage'),
 ]
 
 # Default primary key field type
