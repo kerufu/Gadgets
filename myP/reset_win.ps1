26c20d852626972ceb4c67aa6ebb20e94ca4872b
keru <45701634+kerufu@users.noreply.github.com>
2023-02-18 19:54:39 -0800

    123

--- myP/reset_win.ps1
+++ myP/reset_win.ps1
@@ -1,4 +1,3 @@
-#!/bin/bash
-py manage.py runserver 0.0.0.0:8000 manage.py migrate
-py runserver 0.0.0.0:8000 manage.py makemigrations images
-py manage.py runserver 0.0.0.0:8000 manage.py runserver 0.0.0.0:8000
+py manage.py migrate
+py manage.py makemigrations images
+py manage.py runserver 0.0.0.0:8000
