6d125f5213959eb7e886fd4fc060baf25a8eeac6
kerufu <45701634+kerufu@users.noreply.github.com>
2023-02-18 19:35:56 -0800

    win

--- myP/reset_win.sh
+++ myP/reset_win.sh
@@ -1,3 +1,3 @@
-py manage.py migrate
-py manage.py makemigrations images
-py manage.py runserver 0.0.0.0:8000
+ C:\Users\fkr54\AppData\Local\Programs\Python\Python310\python.exe manage.py runserver 0.0.0.0:8000 manage.py migrate
+ C:\Users\fkr54\AppData\Local\Programs\Python\Python310\python.exe manage.py runserver 0.0.0.0:8000 manage.py makemigrations images
+ C:\Users\fkr54\AppData\Local\Programs\Python\Python310\python.exe manage.py runserver 0.0.0.0:8000 manage.py runserver 0.0.0.0:8000
