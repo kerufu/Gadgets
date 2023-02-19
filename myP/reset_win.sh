ef0763cbf137553192124470bc079200fd6cd8bb
keru <45701634+kerufu@users.noreply.github.com>
2023-02-18 19:42:48 -0800

    bash

--- myP/reset_win.sh
+++ myP/reset_win.sh
@@ -1,3 +1,4 @@
- C:\Users\fkr54\AppData\Local\Programs\Python\Python310\python.exe manage.py runserver 0.0.0.0:8000 manage.py migrate
- C:\Users\fkr54\AppData\Local\Programs\Python\Python310\python.exe manage.py runserver 0.0.0.0:8000 manage.py makemigrations images
- C:\Users\fkr54\AppData\Local\Programs\Python\Python310\python.exe manage.py runserver 0.0.0.0:8000 manage.py runserver 0.0.0.0:8000
+#!/bin/bash
+py manage.py runserver 0.0.0.0:8000 manage.py migrate
+py runserver 0.0.0.0:8000 manage.py makemigrations images
+py manage.py runserver 0.0.0.0:8000 manage.py runserver 0.0.0.0:8000
